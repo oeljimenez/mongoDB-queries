1433c6e3b640bc2d36a112e2e78034ccee727c86
oeljimenez <35323278+oeljimenez@users.noreply.github.com>
2022-07-12 00:35:43 +0200

    Add files via upload

---
+++ Tasca S2.03. Estructura de dades.js
@@ -0,0 +1,27 @@
+
+db.ulleres.insertOne(
+{
+	"proveidor": {
+		"nom": "Opticas España",
+		"adreça": "carrer elipse, 155, 3, 5, Barcelona, 08905, España ",
+		"telefon": "634520654",
+		"fax": "914426216",
+		"nif": "B64222565"
+	},
+	"marca": "Gucci",
+	"graduacio": 2.5,
+	"tipus_muntura": "plastic",
+	"color_muntura": "vermell",
+	"color_vidre": "transparent",
+	"preu": 152.36,
+	"client": {
+		"nom": "John",
+		"adreça_postal": "carrer sant lluis, 10, 3, 1, Barcelona, 08905, España ",
+		"telefon": "933563191",
+		"correu electrònic": "leo_7217@hotmail.com",
+		"data_registre": new ISODate("2022-07-11T09:31:15Z"),
+		"recomanacio": "Pedro",
+		"nom_empleat_venda": "Miguel"
+	}
+}
+)
